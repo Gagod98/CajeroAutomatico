043d5e55d75d40e788626d95c125f60ed05e2df9
Gagod98 <davidsantiagorodriguezbojaca@gmail.com>
2023-09-06 21:19:03 -0500

    agregue cosas al cajero

--- script.js
+++ script.js
@@ -1,13 +1,71 @@
-/**Deberas realizar un semáforo utilizando JS.
-El semáforo deberá cambiar de luz de Verde a Amarilla y de Amarilla a Roja y
- luego de nuevo a Verde.
-Puedes mostrar la imagen del semáforo cambiando, hacer el cambio con 
-console.log o con alert.
-Se debe respetar el orden de los colores:
-De rojo pasa a Verde.
-De amarillo puede pasar a Rojo.
-De Verde pasa a Amarillo. */
 
 
+const input = document.querySelector ('#deposito')
+const botonDeposito = document.querySelector ('#botonDeposito')
+const botonRetiro = document.querySelector ('#botonRetiro')
+const botonConsulta = document.querySelector ('#botonConsulta')
+const dineroEnCuenta = document.querySelector ('#cuentaDinero')
+const saldoInsuficiente = document.querySelector ('#botonError')
+const escondido = document.querySelector ('#botonError span')
+
+const cuentas = {
+    saldo : 100,
+    nombre : "Malinali",
+    id: "001",
+    cuenta: "Malinali",
+    contraseña: "1234"
+}
+// Agrega el saldo con el boton***
+botonDeposito.addEventListener('click', function(e) {
+        const deposito = input.value
+        console.log ({deposito})
+        const valorDeposito = +deposito
+        const saldo = cuentas.saldo
+        const agregarSaldo = (saldo + valorDeposito)
+        dineroEnCuenta.innerText = agregarSaldo
+        cuentas.saldo = agregarSaldo
+        localStorage.setItem('saldo', agregarSaldo)
+})
+// Quita saldo con el boton***
+botonRetiro.addEventListener('click', function(e) {
+    const deposito = input.value
+    const valorDeposito = +deposito
+    const saldo = cuentas.saldo
+    // No deja que el saldo baje de cero y ademas agrega una alerta***
+    if ((valorDeposito > saldo)){ 
+        saldoInsuficiente.classList.remove('esconderNotificacion')
+        saldoInsuficiente.innerHTML = "Saldo insuficiente"
+        cerrarNotificacion()
+        return
+    } 
+    const agregarSaldo = (saldo - valorDeposito)
+    dineroEnCuenta.innerText = agregarSaldo
+    cuentas.saldo = agregarSaldo 
+    localStorage.setItem('saldo', agregarSaldo)
+})
+
+botonConsulta.addEventListener('click', function(e) {
+    const deposito = input.value
+    console.log ({deposito})
+    const valorDeposito = +deposito
+    const saldo = cuentas.saldo
+    const agregarSaldo = (saldo + valorDeposito)
+    dineroEnCuenta.innerText = agregarSaldo
+    cuentas.saldo = agregarSaldo
+})
 
 
+function cerrarNotificacion() {
+    setTimeout (() => {
+        saldoInsuficiente.classList.add('esconderNotificacion')
+    }, 3000)
+}
+
+   // Sirve para cargar todo el DOM se carga TODO***
+
+   addEventListener('DOMContentLoaded', function(){
+
+    const saldoActual = localStorage.getItem ('saldo')
+    cuentas.saldo = +saldoActual
+    saldoInsuficiente.innerHTML = saldoActual
+   })
