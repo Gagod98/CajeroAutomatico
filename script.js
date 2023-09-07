f600e449fe761b860b9e689ca7160f5c446acd8f
Gagod98 <davidsantiagorodriguezbojaca@gmail.com>
2023-09-06 19:25:21 -0500

    Primera fase Cajero Cautomatico

---
+++ script.js
@@ -0,0 +1,13 @@
+/**Deberas realizar un semáforo utilizando JS.
+El semáforo deberá cambiar de luz de Verde a Amarilla y de Amarilla a Roja y
+ luego de nuevo a Verde.
+Puedes mostrar la imagen del semáforo cambiando, hacer el cambio con 
+console.log o con alert.
+Se debe respetar el orden de los colores:
+De rojo pasa a Verde.
+De amarillo puede pasar a Rojo.
+De Verde pasa a Amarillo. */
+
+
+
+
